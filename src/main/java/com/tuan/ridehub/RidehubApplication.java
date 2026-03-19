3da16e6a8973a9b07a96cdd1d850afca893dab5f
Dang Duc Tuan <tdang9078@gmail.com>
2026-03-19 15:43:20 +0700

    init prj

---
+++ src/main/java/com/tuan/ridehub/RidehubApplication.java
@@ -0,0 +1,13 @@
+package com.tuan.ridehub;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RidehubApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RidehubApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/tuan/ridehub/RidehubApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tuan.ridehub;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RidehubApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
