02a2baef3f0e5fc0b876b3cc2c0275d7230c2220
TuanDangDuc <tdang9078@gmail.com>
2026-04-25 13:45:04 +0700

    fix

--- src/main/java/com/tuan/ridehub/controller/UserController.java
+++ src/main/java/com/tuan/ridehub/controller/UserController.java
@@ -101,7 +101,7 @@ public class UserController {
         return userService.getUserById(id);
     }
 
-    @GetMapping("/{username}")
+    @GetMapping("/info/{username}")
     public UserDtopResponse getUserByUsername(
             @PathVariable String username
     ) {
