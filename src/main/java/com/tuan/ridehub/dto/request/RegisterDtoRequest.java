aec66205e087d33578b741489f1bf3f55598c008
TuanDangDuc <tdang9078@gmail.com>
2026-04-25 15:04:35 +0700

    fix err

--- src/main/java/com/tuan/ridehub/dto/request/RegisterDtoRequest.java
+++ src/main/java/com/tuan/ridehub/dto/request/RegisterDtoRequest.java
@@ -4,8 +4,11 @@ import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import lombok.extern.jackson.Jacksonized;
 
 @Data
+@Builder
+@Jacksonized
 public class RegisterDtoRequest {
     String username;
     String password;
