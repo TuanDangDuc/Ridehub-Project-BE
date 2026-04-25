37f1322b90d8ea3cfa08685576faadad96337ab3
TuanDangDuc <tdang9078@gmail.com>
2026-04-25 14:27:29 +0700

    fix dto

--- src/main/java/com/tuan/ridehub/dto/request/RegisterDtoRequest.java
+++ src/main/java/com/tuan/ridehub/dto/request/RegisterDtoRequest.java
@@ -1,10 +1,14 @@
 package com.tuan.ridehub.dto.request;
 
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
 @Builder
+@NoArgsConstructor
+@AllArgsConstructor
 public class RegisterDtoRequest {
     String username;
     String password;
